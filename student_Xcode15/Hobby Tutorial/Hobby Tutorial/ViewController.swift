08f27234a87c7086978e47cd5714ba88aafafcdd
Cindy <156014828+lecin91@users.noreply.github.com>
2024-12-01 14:49:59 -0500

    Lab: Tutorial Screen

---
+++ student_Xcode15/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by remotestudent on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

0f824df6e9d070327d51d925a47c254676ff220e
Cindy <156014828+lecin91@users.noreply.github.com>
2024-12-01 17:26:44 -0500

    Lab: Basic Interactions

---
+++ student_Xcode15/Two Buttons/Two Buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by remotestudent on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var clearTextButtonTapped: UIButton!
+    @IBOutlet var setTextButtonTapped: UIButton!
+    
+    
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

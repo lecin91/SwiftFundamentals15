145dc0399789c6f4dbbd4d35a3890f56cf759d04
Cindy <156014828+lecin91@users.noreply.github.com>
2024-12-08 23:41:09 -0500

    Guide Project 2

---
+++ student_Xcode15/Apple Pie/Apple Pie/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Apple Pie
+//
+//  Created by remotestudent on 12/8/24.
+//
+
+import UIKit
+
+class viewController: UIViewController {
+
+
+}
+
---
+++ student_Xcode15/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by remotestudent on 12/8/24.
+//
+
+
+
